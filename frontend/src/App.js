bedb6dffd26c1817a5cb0c15659145353a0532ba
PORTORREAL <portorreal@ad25wv020h7jy.myfiosgateway.com>
2022-05-27 15:57:15 -0400

    fixed typo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
-return (
-  <h1>Test Title by Ryan</h1>
-);
+const App = () => {
+  return (
+    <h1>Test Title by Ryan</h1>
+  );
+};
 
 export default App;
