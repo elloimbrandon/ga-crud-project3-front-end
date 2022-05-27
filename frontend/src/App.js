59e7e2d3f0694808395c4f922d47d673f92a6c51
Tan Le <tanle@Tans-MBP.hsd1.fl.comcast.net>
2022-05-27 16:43:07 -0400

    trying to deploy to heroku

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
+
   return (
-<<<<<<< HEAD
     <>
-      <h1>Hello World</h1>
+    <h1>Test Title by Ryan</h1>
+    <h2>Hello</h2>
     </>
   )
+
 }
-=======
-    <h1>Test Title by Ryan</h1>
-  );
-};
->>>>>>> bedb6dffd26c1817a5cb0c15659145353a0532ba
+
 
 export default App;
